16bfd80a11c371764ecb9d3f382bd5cd6964fdcd
Zach Allen <zachjallen@gmail.com>
2018-01-23 21:22:13 -0400

    added get one person route

--- app/routes/people_routes.js
+++ app/routes/people_routes.js
@@ -1,5 +1,15 @@
+var ObjectID = require('mongodb').ObjectID;
 
 module.exports = function(app, db) {
+
+  app.get('/people/:id', (req, res) => {
+    const details = { '_id': new ObjectID(req.params.id)};
+    db.collection('people').findOne(details, (err, item) => {
+      if (err) res.send({'error': 'An error has occurred'});
+      else res.send(item);
+    });
+  });
+
   app.post('/people', (req, res) => {
     const person = {
       firstname: req.body.firstname,
